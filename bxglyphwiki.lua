8ceec64c6699d6c3e0c1f0a9e034c9cf2c45e702
Takayuki YATO <tak_yato@yahoo.co.jp>
2020-09-02 07:40:03 +0900

    Documentation (v0.6)

--- bxglyphwiki.lua
+++ bxglyphwiki.lua
@@ -9,7 +9,7 @@ end
 local M = bxglyphwiki
 ---------------------------------------- interfaces
 M.prog_name = "bxglyphwiki"
-M.version = "0.6-pre"
+M.version = "0.6"
 M.mod_date = "2020/09/01"
 M.url_json = "http://non-ssl.glyphwiki.org/json?name=%s"
 M.url_svg = "http://non-ssl.glyphwiki.org/glyph/%s@%d.svg"
